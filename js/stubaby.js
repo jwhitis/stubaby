7a149335c14ce34bbe33d6c6ae5a12f6d6a70873
jwhitis <jack.whitis@gmail.com>
2014-10-05 12:50:09 -0500

    Implement Masonry to tile photos

--- js/stubaby.js
+++ js/stubaby.js
@@ -1,28 +1,39 @@
 $(document).on("ready", function() {
 
-  // Flickr account ID
-  var flickr_id = "75673948@N07";
-
   // Fetch public photos from Flickr
   var flickr_url = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  var flickr_id = "75673948@N07"; // Change this to your Flickr account ID
 
   $.getJSON(flickr_url, {
     id: flickr_id,
     format: "json"
   }).done(function(data) {
-    console.log(data);
+    var photos = data.items;
 
-    // Create an image tag for each returned photo
-    $.each(data.items, function(index, item) {
-      var image_url = item.media.m;
+    // Add an image element for each returned photo
+    $.each(photos, function(index, photo) {
+
+      // Modify image URL to fetch correct image size
+      var image_url = photo.media.m;
       var flickr_regexp = /(http:\/\/farm\d+.staticflickr.com\/\d+\/\d+_[0-9A-z]+_)[\w-](\.\w+)/;
-      var matches = flickr_regexp.exec(image_url);
-      image_url = matches[1] + "n" + matches[2];
+      var url_pieces = flickr_regexp.exec(image_url);
+      image_url = url_pieces[1] + "n" + url_pieces[2];
+
       var image_tag = "<img src=\"" + image_url + "\" />"
-      $("div#main").append(image_tag);
+      var image_container = "<div class=\"photo\">" + image_tag + "</div>"
+      $("div#photos").append(image_container);
+
+      // Once all image elements have been added to page
+      if (index == (photos.length - 1)) {
+
+        // Initialize Masonry after all images have loaded
+        $("div#photos").imagesLoaded(function() {
+          $("div#photos").masonry({
+            itemSelector: "div.photo",
+            gutter: 15
+          });
+        });
+      }
     });
   });
 });
-
-// Flickr Photo URL Format:
-// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
